3939e0856910f0dcaf02742c95de07836d0a9bfd
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-03-14 00:12:02 -0500

    packages added and start styles and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
 import './App.css'
-import Note from './components/notes/Note'
-import mockNotes from './mocks/notes.json'
 import Counter from './components/counters/Counter'
-import { NoteModel } from './models/note'
+import WelcomePage from './components/pages/WelcomePage'
+import SignInPage from './components/pages/SignInPage'
 
-const mockNote:NoteModel = {
-  _id: '213',
-  title: 'Hola',
-  createdAt: '123123123',
-  updatedAt: '2343242423',
-  text: 'Mo'
-}
 function App() {
-const [notes, setNotes] = useState<NoteModel[]>([mockNote])
   return (
-    <div className="App">
-      <Note note={notes[0]}/>
+    <>
+      {/* <WelcomePage /> */}
+      <SignInPage/>
       <Counter/>
-    </div>
+    </>
   )
 }
 
--- frontend/src/components/counters/Counter.tsx
+++ frontend/src/components/counters/Counter.tsx
@@ -2,9 +2,10 @@ import Countdown from "react-countdown";
 
 export default function Counter() {
   const iLeave = new Date('04/05/2023')
+  iLeave.setHours(7)
   return (
     <>
-    <h1>Time left </h1>
+    {/* <h1>Time left </h1> */}
     <h3><Countdown date={iLeave} /></h3>
     </>
   );
