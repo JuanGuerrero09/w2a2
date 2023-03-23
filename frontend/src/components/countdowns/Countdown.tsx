6cb4eae54717d827f960fb97f6cba47cbd959fb0
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-03-22 23:57:55 -0500

    create countdown component

---
+++ frontend/src/components/countdowns/Countdown.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {Carousel, Card} from 'react-bootstrap';
+
+export default function Countdown() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel variant="dark" activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <Card className='p-5' >
+        <h1>I'm a carousel example</h1>
+
+        </Card>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <h1>I'm a carousel example</h1>
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <h1>I'm a carousel example</h1>
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
---
+++ frontend/src/models/countdown.ts
@@ -0,0 +1,6 @@
+export interface CountdownModel {
+    _id: string,
+    title: string,
+    day: string,
+    createdAt: string,
+}
