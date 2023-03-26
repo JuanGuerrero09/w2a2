4025ca8b72c32cf3e9a4b45cf4a9a80848c732e6
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-03-26 11:12:13 -0500

    delete functionality added

--- frontend/src/components/canvas/CanvasSketch.tsx
+++ frontend/src/components/canvas/CanvasSketch.tsx
@@ -1,23 +1,21 @@
-import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
+import { ChangeEvent, useContext, useRef, useState } from "react";
 import { Button, ButtonGroup, Form } from "react-bootstrap";
-import CanvasStyles from "../../styles/Canvas.module.css";
-import { ChangeEvent, useContext, useEffect, useRef, useState } from "react";
+import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 import { AppContext } from "../../context/AppContext";
-import { DrawModel } from "../../models/draw";
+import { IoMdUndo, IoMdRedo } from 'react-icons/io'
+import { BsEraser, BsPencil } from 'react-icons/bs'
+import CanvasStyles from "../../styles/Canvas.module.css";
 
 export default function CanvasSketch() {
   const { drawsContext } = useContext(AppContext)
-  const { draws, getDraws, deleteDraw, createDraw } = drawsContext
-  console.log(draws)
+  const { createDraw } = drawsContext
   const [strokeSize, setStrokeSize] = useState(15);
   const [canvasSize, setCanvasSize] = useState(400);
   const [color, setColor] = useState("#000");
+  const [isErasing, setIsErasing] = useState(false);
   const sketch = useRef<ReactSketchCanvasRef>(null);
-  const [imgArr, setImgArr] = useState<string[]>([])
 
-  useEffect(() => {
-    getDraws()
-  }, [])
+  
 
   const strokeSizeChange = (e: ChangeEvent<HTMLInputElement>) => {
     const currentValue = Number(e.target.value);
@@ -32,6 +30,15 @@ export default function CanvasSketch() {
     const draw = sketch.current
     if (draw !== null) {
       draw.undo();
+    }
+  };
+
+  const toggleEraser = () => {
+    const draw = sketch.current
+    const eraserToggle = !isErasing
+    if (draw !== null) {
+      draw.eraseMode(!isErasing);
+      setIsErasing(eraserToggle)
     }
   };
   const redoDraw = () => {
@@ -53,45 +60,41 @@ export default function CanvasSketch() {
       const imgSaved = await drawIMG.exportImage("png");
       await createDraw({img: imgSaved})
       drawIMG.clearCanvas()
-      setImgArr([...imgArr, imgSaved])
-      console.log(await imgArr)
     }
   };
 
   return (
     <>
     <section className={CanvasStyles.CanvasDrawSection}>
-      <Form.Label>StrokeSize: {strokeSize}</Form.Label>
+      <Form.Label>{isErasing? 'Eraser': 'Pencil'} Size: {strokeSize}</Form.Label>
       <Form.Range
-      style={{width: '350px'}}
+      style={{width: '250px'}}
         min={1}
         max={30}
         defaultValue={15}
         onChange={strokeSizeChange}
       />
+      <div className={CanvasStyles.CanvasBtnSection}>
       <Form.Control type="color" onChange={colorChange} />
-      <ButtonGroup>
-        <Button onClick={undoDraw}>Undo</Button>
-        <Button onClick={redoDraw}>Redo</Button>
         <Button onClick={clearCanvas}>Clear</Button>
-        {/* <Button onClick={saveImg}>Save Image</Button> */}
+      <ButtonGroup>
+        <Button onClick={undoDraw}><IoMdUndo/></Button>
+        <Button onClick={redoDraw}><IoMdRedo/></Button>
       </ButtonGroup>
+        <Button onClick={toggleEraser}>{isErasing ? <BsEraser/> : <BsPencil/>}</Button>
+      </div>
       <ReactSketchCanvas
         className={CanvasStyles.CanvasDraw}
         width={`${canvasSize}px`}
         height={`${canvasSize}px`}
         strokeWidth={strokeSize}
+        eraserWidth={strokeSize}
         strokeColor={color}
         ref={sketch}
         />
     </section>
         <Button onClick={saveIMG}>Save Image</Button>
-        <h2>Our Drawings</h2>
-      <div className={CanvasStyles.ImgSection}>
-      {draws && draws.map((draw:DrawModel) => {
-          return (<img className={CanvasStyles.Img} src={draw.img} key={draw._id} width='200px'/>)
-        })}
-      </div>
+        
         </>
   );
 }
--- frontend/src/pages/CanvasPage.tsx
+++ frontend/src/pages/CanvasPage.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
 import CanvasSketch from "../components/canvas/CanvasSketch";
-import CanvasStyles from '../styles/Canvas.module.css'
+import CanvasStyles from "../styles/Canvas.module.css";
+import { AppContext } from "../context/AppContext";
+import { DrawModel } from "../models/draw";
+import { MdDelete } from "react-icons/md";
 
 export default function CanvasPage() {
+  const { drawsContext } = useContext(AppContext);
+  const { draws, getDraws, deleteDraw } = drawsContext;
+
+  useEffect(() => {
+    getDraws();
+  }, []);
+
   return (
     <main className={CanvasStyles.CanvasPage}>
-        <CanvasSketch />
+      <CanvasSketch />
+      <h2>Our Drawings</h2>
+      <div className={CanvasStyles.ImgSection}>
+        {draws &&
+          draws.map((draw: DrawModel) => {
+            return (
+              <div className={CanvasStyles.ImgHolder}>
+                <img
+                  className={CanvasStyles.Img}
+                  src={draw.img}
+                  key={draw._id}
+                  width="200px"
+                ></img>
+                <MdDelete className={CanvasStyles.Delete} onClick={() => {
+                  deleteDraw(draw._id)
+                }} color="red"/>
+              </div>
+            );
+          })}
+      </div>
     </main>
-  )
+  );
 }
