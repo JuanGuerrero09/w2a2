0ab2c64f50b1624ccfde8fe5ae4b2420d7a88f1c
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-03-28 23:55:32 -0500

    Home screen without partner created

---
+++ frontend/src/pages/HomeWithoutPartner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HomeWithoutPartner() {
+  return (
+    <>
+      <div>HomeWithoutPartner</div>
+      <form >
+        <input type="text" />
+        <button>Add Partner</button>
+      </form>
+    </>
+  );
+}
