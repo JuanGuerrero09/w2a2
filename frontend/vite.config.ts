a5740dbae42f67f71471b66fa85b3d284aafb967
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-04-03 23:44:43 -0500

    added published web api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000/'
+      '/api': 'https://w2a2-api.onrender.com/'
     }
   },
   plugins: [react()],
